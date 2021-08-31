8a3f296e8fb63173227127253f3c473b5a43c965
Albin Ljungdahl <Albin.Ljungdal@se.experis.com>
2021-08-31 09:27:48 +0200

    chore: Initial commit

---
+++ src/main/java/se/experis/humans_vs_zombies/HumansVsZombiesApplication.java
@@ -0,0 +1,13 @@
+package se.experis.humans_vs_zombies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumansVsZombiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumansVsZombiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/experis/humans_vs_zombies/HumansVsZombiesApplicationTests.java
@@ -0,0 +1,13 @@
+package se.experis.humans_vs_zombies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumansVsZombiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
