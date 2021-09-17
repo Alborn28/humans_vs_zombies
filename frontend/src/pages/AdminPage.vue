7cccf7bead69b9a14954922c48d1efb346659bfe
aljosa_vuckovic <aljosa.vuckovic@se.experis.com>
2021-09-17 13:30:06 +0200

    ´feat:started on the admin page

---
+++ frontend/src/pages/AdminPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+      <select name="" id="" v-for="game in fetchGames()">
+          <option>
+              game 1
+          </option>
+          <option>
+              game 2
+          </option>
+      </select>
+  </div>
+</template>
+
+<script>
+import {  mapActions, mapGetters, mapState } from 'vuex';
+export default {
+
+    computed:{
+        ...mapGetters(["isAdmin"]),
+        ...mapState(["games"])
+    },
+    methods:{
+        ...mapActions(["fetchGames"])
+    },
+   async created(){  
+        if (!this.isAdmin) {
+      this.$router.push("/");
+        }
+        await this.fetchGames();
+    },
+
+
+
+}
+</script>
+
+<style>
+
+</style>
